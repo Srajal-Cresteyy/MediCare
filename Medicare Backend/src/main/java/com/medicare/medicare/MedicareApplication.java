551999ccbc70886a67835258c76873d883ade9f4
Srajal Dwivedi <srajal72dwivedi@gmail,com>
2024-08-09 00:48:43 +0530

    Configurations for postgres sql database to my application

--- Medicare Backend/src/main/java/com/medicare/medicare/MedicareApplication.java
+++ Medicare Backend/src/main/java/com/medicare/medicare/MedicareApplication.java
@@ -1,5 +1,6 @@
 package com.medicare.medicare;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicareApplication {
 
 	public static void main(String[] args) {
+//		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(MedicareApplication.class, args);
 	}
 
