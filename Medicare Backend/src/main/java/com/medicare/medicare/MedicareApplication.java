ca88f69423700b992dd2d65a1f0dea01eae7777d
Srajal Dwivedi <srajal72dwivedi@gmail.com>
2024-08-18 00:38:42 +0530

    Wrote Configurations on spring boot so that frontend can easily interact with the backend

--- Medicare Backend/src/main/java/com/medicare/medicare/MedicareApplication.java
+++ Medicare Backend/src/main/java/com/medicare/medicare/MedicareApplication.java
@@ -1,6 +1,5 @@
 package com.medicare.medicare;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MedicareApplication {
 
 	public static void main(String[] args) {
-//		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(MedicareApplication.class, args);
 	}
 
--- Medicare Backend/src/main/java/com/medicare/medicare/conf/WebConfig.java
+++ Medicare Backend/src/main/java/com/medicare/medicare/conf/WebConfig.java
@@ -1,2 +1,49 @@
-package com.medicare.medicare.conf;public class WebConfig {
+package com.medicare.medicare.conf;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import java.util.Arrays;
+
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = getCorsConfiguration();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterRegistrationBean.setOrder(-102); //  Putting this bean at the lowest order to be used before any spring security filter
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public CorsConfiguration getCorsConfiguration() {
+        // CORS : Cross Origin Resource Sharing
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // Frontend Will send some Credentials which should be accepted
+        config.addAllowedOrigin("http://localhost:5173");
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT
+        ));
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()
+        ));
+        config.setMaxAge(3600L); // 30 Minutes
+        return config;
+    }
 }
